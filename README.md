32cc30c499751f0ad0ff83bef4b0d0aad1a8d922
IronLung <hwlooverhello@gmail.com>
2023-12-21 17:56:13 +0530

    Initial Commit v2

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# automatedDripIrrigation
+# Configuration
+
+## To Change any parameter change the following defines
+
+```cpp
+// Comment out these lines to disable module
+#define USE_EXTERNAL_RTC                        // Use an external i2c Real Time Clock to fetch time (NOTE: Requires a relay)
+#define USE_OLED                                // Use an OLED display (NOTE: Requires an OLED)
+#define DEF_SSID "<SSID to try to connect to>"    // Default Wi-Fi Name
+#define DEF_PASS "<Passkey to DEF_SSID>"          // Password for Wi-Fi name
+```
--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,37 @@
+
+/**
+ * CONFIGURATION:
+ * Change as per need
+ * Comment out any one to disable module
+ **/
+
+#define USE_NTP	 // Use the Network Time Protocol to sync time
+#define USE_OLED // Use an OLED display (NOTE: Requires an OLED)
+#define USE_WIFI // Enable web-server and WiFi connection (Required by NTP)
+
+#if defined(USE_NTP) && !defined(USE_WIFI)
+#error "The NTP module requires WiFi, please enable the wifi module, or disable the NTP module"
+#endif
+
 #include <Arduino.h>
+
+#ifdef USE_OLED
 #include <U8g2lib.h>
+#endif
+
 #include <ESP32Time.h>
+
 #include <ArduinoJson.h>
 
 #include <WiFi.h>
-#include <WiFiClient.h>
-#include <WiFiManager.h>
+// #include <WiFiClient.h>
+// #include <WiFiManager.h>
 #include <ESPAsyncWebServer.h>
 #include <SPIFFS.h>
 
 #include <vector>
 #include "time.h"
+#include <RTClib.h>
 
 #include "spiffsHelper.hpp"
 
@@ -24,28 +45,41 @@
 #define RELAY 4
 #define BUZZER 26
 
-
 #define DEFAULT_DURATION 7000 // in ms
+#define JSON_LOCATION "/Alarms.json"
+
+#undef U8G2_WITH_CLIP_WINDOW_SUPPORT
+#undef U8G2_WITH_FONT_ROTATION
+#undef U8G2_WITH_UNICODE
+#undef U8G2_WITH_INTERSECTION
+#undef U8G2_WITH_INTERSECTION
+#undef U8G2_WITH_HVLINE_SPEED_OPTIMIZATION
+#undef U8G2_WITH_FONT_ROTATION
+
 using std::sort;
 using std::vector;
 
+#ifdef USE_OLED
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
+#endif
+
 ESP32Time rtc;
 AsyncWebServer server(80);
-WiFiManager wm;
+// WiFiManager wm;
 
 tm nextAlarm;
 vector<tm> Alarms = {};
 
-struct Alarm {
+struct Alarm
+{
 	tm time;
 	time_t timeAdded;
 }; // A structure for Alarms
 
-/// @brief A function to init the tm structure 
-/// @param hours 
-/// @param mins 
-/// @param secs 
+/// @brief A function to init the tm structure
+/// @param hours
+/// @param mins
+/// @param secs
 /// @return tm structure
 tm its(int hours, int mins, int secs) // InitTimeStructure
 {
@@ -57,9 +91,10 @@ tm its(int hours, int mins, int secs) // InitTimeStructure
 	return x;
 }
 
+#ifdef USE_OLED
 /// @brief             Method to easily print to display
 /// @param align       The x coordinate. Send ALIGN_CENTER or ALIGN_RIGHT or ALIGN_LEFT for predefined alignments
-/// @param y           The y coordinate 
+/// @param y           The y coordinate
 /// @param text        Text to print
 /// @param font        Font to use
 /// @param clear       Clear the display?
@@ -76,13 +111,13 @@ void fastPrint(int align, int y, String text, const uint8_t *font = u8g2_font_nc
 	switch (align)
 	{
 	case ALIGN_CENTER:
-		u8g2.setCursor((width - u8g2.getUTF8Width(text.c_str())) / 2, y);		
+		u8g2.setCursor((width - u8g2.getUTF8Width(text.c_str())) / 2, y);
 		break;
 	case ALIGN_LEFT:
 		u8g2.setCursor(0, y);
 		break;
 	case ALIGN_RIGHT:
-		u8g2.setCursor(width -  u8g2.getUTF8Width(text.c_str()), y);
+		u8g2.setCursor(width - u8g2.getUTF8Width(text.c_str()), y);
 		break;
 	default:
 		u8g2.setCursor(align, y);
@@ -96,33 +131,41 @@ void fastPrint(int align, int y, String text, const uint8_t *font = u8g2_font_nc
 	}
 }
 
-/// @brief  Get formated time	
+#else
+void fastPrint(int align, int y, String text, const uint8_t *font = 0, bool clear = true, bool sendBuffer = false)
+{
+}
+const uint8_t *u8g2_font_ncenB08_tr = 0;
+
+#endif
+
+/// @brief  Get formated time
 /// @return String containing H:M:S
 inline String getTimeF()
 {
 	return (rtc.getTime("%H:%M:%S"));
 }
 
-/// @brief           Compare two tm structures	
-/// @param a         Pointer to one tm structure 
+/// @brief           Compare two tm structures
+/// @param a         Pointer to one tm structure
 /// @param b         Pointer to second tm structure
 /// @param threshold Threshold for comparing
-/// @return 
-bool compareAlarms(const tm &a, const tm &b, int threshold = 0)
+/// @return
+inline bool compareAlarms(const tm &a, const tm &b, int threshold = 0)
 {
-    if (abs(a.tm_hour - b.tm_hour) > threshold)
-        return false;
-    else if (abs(a.tm_min - b.tm_min) > threshold)
-        return false;
-    else if (abs(a.tm_sec - b.tm_sec) > threshold)
-        return false;
-    else
-        return true;
+	if (abs(a.tm_hour - b.tm_hour) > threshold)
+		return false;
+	else if (abs(a.tm_min - b.tm_min) > threshold)
+		return false;
+	else if (abs(a.tm_sec - b.tm_sec) > threshold)
+		return false;
+	else
+		return true;
 }
 /// @brief Returns the closest Alarm
-/// @param now Current time 
+/// @param now Current time
 /// @param alarms Pointer to vector of current alarms
-/// @return 
+/// @return
 tm getNextAlarm(const tm &now, const vector<tm> &alarms)
 {
 	if (alarms.empty())
@@ -177,11 +220,11 @@ tm getNextAlarm(const tm &now, const vector<tm> &alarms)
 }
 
 /// @brief Load alarms from SPIFFS
-/// @param alarms Pointer to the vector that should hold all of the alarms 
+/// @param alarms Pointer to the vector that should hold all of the alarms
 void loadAlarms(vector<tm> *alarms)
 {
-	StaticJsonDocument<2048> doc;
-	deserializeJson(doc, SpiffsHelper::readFile("/Alarms.json"));
+	DynamicJsonDocument doc(512);
+	deserializeJson(doc, SpiffsHelper::readFile(JSON_LOCATION));
 	JsonArray arr = doc.as<JsonArray>();
 
 	for (JsonVariant i : arr)
@@ -192,78 +235,55 @@ void loadAlarms(vector<tm> *alarms)
 	}
 }
 
-
-/// @brief Append an alarm, by updating SPIFFS & the internal vector
-/// @param alarm The alarm that is to be appended
-/// @param alarms Pointer to the vector that holds all of the alarms
-void appendAlarm(tm *alarm, vector<tm> *alarms)
+void setSystemTimeFromRTC()
 {
-    // Add the new alarm to the vector
-    alarms->push_back(*alarm);
+	RTC_DS3231 _rtc;
+	_rtc.begin();
+	// _rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
 
-    // Save the updated vector to the JSON file
-    DynamicJsonDocument doc(2048);
-    JsonArray arr = doc.to<JsonArray>();
-    
-    for (const auto a : *alarms)
-    {
-        JsonObject obj = arr.createNestedObject();
-        obj["tm_hour"] = a.tm_hour;
-        obj["tm_min"] = a.tm_min;
-        obj["tm_sec"] = a.tm_sec;
-    }
+	DateTime now = _rtc.now();
+	struct tm tmStruct;
 
-    String jsonContents;
-    serializeJson(doc, jsonContents);
+	tmStruct.tm_year = now.year() - 1900;
+	tmStruct.tm_mon = now.month() - 1;
+	tmStruct.tm_mday = now.day();
+	tmStruct.tm_hour = now.hour();
+	tmStruct.tm_min = now.minute();
+	tmStruct.tm_sec = now.second();
 
-    SpiffsHelper::writeFile("/Alarms.json", jsonContents);
+	time_t time = mktime(&tmStruct);
+	struct timeval tv = {time, 0};
+
+	settimeofday(&tv, nullptr);
+
+	Serial.println("Time set from RTC!");
 }
 
-void setup()
+void setRTCTimeFromNTP()
 {
-	// Setup the pins 
-	pinMode(RELAY, OUTPUT);
-	pinMode(BUZZER, OUTPUT);
-	digitalWrite(RELAY, HIGH);
-	digitalWrite(BUZZER, HIGH);
+	RTC_DS3231 _rtc; 
 
-	u8g2.begin(); // Start the OLED display
-	Serial.begin(115200);
-	digitalWrite(BUZZER, LOW);
-
-	fastPrint(ALIGN_LEFT, 10, "Starting Setup", u8g2_font_ncenB08_tr, true, true);
-
-	// wm.resetSettings(); // Reset previously saved WiFi credentials (Just for testing)
-	wm.setHttpPort(80); // Set the WiFi manager port to 80
-	wm.setDarkMode(true);
-	
-	wm.setAPCallback([](WiFiManager *wm) {
-		fastPrint(ALIGN_LEFT, 10, "Using configPortal", u8g2_font_ncenB08_tr, true, false);
-		fastPrint(ALIGN_LEFT, 21, String("AP: ESP32 wm"), u8g2_font_ncenB08_tr, false, false);
-		fastPrint(ALIGN_LEFT, 32, "IP: 192.168.4.1", u8g2_font_ncenB08_tr, false, true);
-		// Serial.println("UwU");
-	});
-	bool usedConfigPortal = false;
-	wm.setSaveConfigCallback([&usedConfigPortal]() {
-		usedConfigPortal = true;
-	});
-	
-	if(wm.autoConnect("ESP32 wm"))
+	if (!_rtc.begin())
 	{
-		// Connected to wifi
-		if (usedConfigPortal) ESP.restart();
-		else {
-			fastPrint(ALIGN_LEFT, 21, "Connected to WiFi", u8g2_font_ncenB08_tr, false, true);
-		}
+		Serial.println("Couldn't find RTC");
+		while (1)
+			;
 	}
 
-	digitalWrite(BUZZER, HIGH);
-	delay(100);
-	digitalWrite(BUZZER, LOW);
+	// Connect to WiFi
+	WiFi.disconnect();
+	WiFi.mode(WIFI_STA);
+	WiFi.begin("Hobbiton", "taxicab1729");
 
-	fastPrint(ALIGN_LEFT, 32, "Setting Time", u8g2_font_ncenB08_tr, false, true);
-	
-	delay(1000);
+	// Wait for WiFi connection
+	while (WiFi.status() != WL_CONNECTED)
+	{
+		delay(1000);
+		Serial.println("Connecting to WiFi...");
+	}
+	Serial.println("Connected to WiFi");
+
+	// Set the time zone to UTC+5:30
 	configTime(19800, 0, "pool.ntp.org");
 	struct tm timeinfo;
 	if (getLocalTime(&timeinfo))
@@ -271,26 +291,120 @@ void setup()
 		rtc.setTimeStruct(timeinfo);
 	}
 
+	tm now = rtc.getTimeStruct();
+
+	// Adjust the RTC time
+	Serial.println(getTimeF());
+	_rtc.adjust(DateTime(now.tm_year + 1900, now.tm_mon + 1, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec));
+	Serial.println("RTC time set from NTP!");
+}
+
+/// @brief Append an alarm, by updating SPIFFS & the internal vector
+/// @param alarm The alarm that is to be appended
+/// @param alarms Pointer to the vector that holds all of the alarms
+void appendAlarm(tm *alarm, vector<tm> *alarms)
+{
+	// Add the new alarm to the vector
+	alarms->push_back(*alarm);
+
+	// Save the updated vector to the JSON file
+	DynamicJsonDocument doc(512);
+	JsonArray arr = doc.to<JsonArray>();
+
+	for (const auto a : *alarms)
+	{
+		JsonObject obj = arr.createNestedObject();
+		obj["tm_hour"] = a.tm_hour;
+		obj["tm_min"] = a.tm_min;
+		obj["tm_sec"] = a.tm_sec;
+	}
+
+	String jsonContents;
+	serializeJson(doc, jsonContents);
+
+	SpiffsHelper::writeFile(JSON_LOCATION, jsonContents);
+}
+
+void setup()
+{
+	// Setup the pins
+	pinMode(RELAY, OUTPUT);
+	pinMode(BUZZER, OUTPUT);
+	digitalWrite(RELAY, HIGH);
+	digitalWrite(BUZZER, HIGH);
+#ifdef USE_OLED
+	u8g2.begin(); // Start the OLED display
+	fastPrint(ALIGN_LEFT, 10, "Starting Setup", u8g2_font_ncenB08_tr, true, true);
+#endif
+	Serial.begin(115200);
+	digitalWrite(BUZZER, LOW);
+
+	// wm.resetSettings(); // Reset previously saved WiFi credentials (Just for testing)
+	/* 	wm.setHttpPort(80); // Set the WiFi manager port to 80
+		wm.setDarkMode(true);
+
+		wm.setAPCallback([](WiFiManager *wm)
+						 {
+							 fastPrint(ALIGN_LEFT, 10, "Using configPortal", u8g2_font_ncenB08_tr, true, false);
+							 fastPrint(ALIGN_LEFT, 21, String("AP: ESP32 wm"), u8g2_font_ncenB08_tr, false, false);
+							 fastPrint(ALIGN_LEFT, 32, "IP: 192.168.4.1", u8g2_font_ncenB08_tr, false, true);
+							 // Serial.println("UwU");
+						 });
+		bool usedConfigPortal = false;
+		wm.setSaveConfigCallback([&usedConfigPortal]()
+								 { usedConfigPortal = true; });
+
+		if (wm.autoConnect("ESP32 wm"))
+		{
+			// Connected to wifi
+			if (usedConfigPortal)
+				ESP.restart();
+			else
+			{
+				fastPrint(ALIGN_LEFT, 21, "Connected to WiFi", u8g2_font_ncenB08_tr, false, true);
+			}
+		} */
+
+
+	WiFi.mode(WIFI_AP);
+	WiFi.softAP("Drip Irrigation");
+
+	digitalWrite(BUZZER, HIGH);
 	delay(100);
+	digitalWrite(BUZZER, LOW);
+
+	fastPrint(ALIGN_LEFT, 32, "Setting Time", u8g2_font_ncenB08_tr, false, true);
+
+	/* 	delay(1000);
+		configTime(19800, 0, "pool.ntp.org");
+		struct tm timeinfo;
+		if (getLocalTime(&timeinfo))
+		{
+			rtc.setTimeStruct(timeinfo);
+		} */
+
+	setSystemTimeFromRTC();
+
+	// delay(100);
 	fastPrint(ALIGN_LEFT, 43, "Starting SPIFFS", u8g2_font_ncenB08_tr, false, true);
 
-    if (!SPIFFS.begin(true)) {
-        Serial.println("An error occurred while mounting SPIFFS");
-        return;
-    }
+	if (!SPIFFS.begin(true))
+	{
+		Serial.println("An error occurred while mounting SPIFFS");
+		return;
+	}
 
 	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request)
-	{
+			  {
 		String htmlContent = SpiffsHelper::readFile("/WebPages/root.html");
 		if (htmlContent.isEmpty())
 		{
 			request->send(500, "text/html", "<h1>Internal Server Error</h1>");
 		}
-		request->send(200, "text/html", htmlContent);
-	});
+		request->send(200, "text/html", htmlContent); });
 
 	server.on("/submit", HTTP_POST, [](AsyncWebServerRequest *request)
-	{
+			  {
 		String hours = request->arg("hours");
 		String mins = request->arg("mins");
 		String seconds = request->arg("seconds");
@@ -301,10 +415,10 @@ void setup()
 		nextAlarm = getNextAlarm(rtc.getTimeStruct(), Alarms);
 
 		// Send a redirect response to the main page
-		request->redirect("/");
-	});
+		request->redirect("/"); });
 
-	server.on("/remove_alarm", HTTP_POST, [](AsyncWebServerRequest *request) {
+	server.on("/remove_alarm", HTTP_POST, [](AsyncWebServerRequest *request)
+			  {
 		String indexStr = request->arg("index");
 		int index = indexStr.toInt();
 
@@ -312,7 +426,7 @@ void setup()
 			Alarms.erase(Alarms.begin() + index);
 
 			// Update the Alarms.json file
-			DynamicJsonDocument doc(2048);
+			DynamicJsonDocument doc(512);
 			JsonArray arr = doc.to<JsonArray>();
 
 			for (const auto &alarm : Alarms) {
@@ -325,7 +439,7 @@ void setup()
 			String jsonContents;
 			serializeJson(doc, jsonContents);
 
-			SpiffsHelper::writeFile("/Alarms.json", jsonContents);
+			SpiffsHelper::writeFile(JSON_LOCATION, jsonContents);
 
 			// Update nextAlarm
 			nextAlarm = getNextAlarm(rtc.getTimeStruct(), Alarms);
@@ -334,12 +448,10 @@ void setup()
 			request->redirect("/");
 		} else {
 			request->send(400, "application/json", "{\"success\": false, \"error\": \"Invalid index\"}");
-		}
-	});
-
+		} });
 
 	server.on("/alarms", HTTP_GET, [](AsyncWebServerRequest *request)
-	{
+			  {
 		// Convert alarms vector to JSON format
 		String jsonAlarms = "[";
 		for (const auto &alarm : Alarms)
@@ -353,29 +465,20 @@ void setup()
 		jsonAlarms.remove(jsonAlarms.length() - 1); // Remove the trailing comma
 		jsonAlarms += "]";
 
-		request->send(200, "application/json", jsonAlarms);
-	});
+		request->send(200, "application/json", jsonAlarms); });
 
-	server.on("/isAlarm", HTTP_GET, [](AsyncWebServerRequest *request) {
-		request->send(200, "text/plain", compareAlarms(rtc.getTimeStruct(), nextAlarm, DEFAULT_DURATION) ? "YES" : "NO");
-	});
+	server.on("/isAlarm", HTTP_GET, [](AsyncWebServerRequest *request)
+			  { request->send(200, "text/plain", compareAlarms(rtc.getTimeStruct(), nextAlarm, DEFAULT_DURATION) ? "YES" : "NO"); });
 
-	server.on("/time", HTTP_GET, [](AsyncWebServerRequest *request) {
-		request->send(200, "text/plain", getTimeF());
-	});
-	
-	server.on("/favicon.ico", HTTP_ANY, [](AsyncWebServerRequest *request) {
-		request->redirect("https://static.wikia.nocookie.net/minecraft_gamepedia/images/d/dc/Water_Bucket_JE2_BE2.png/revision/latest/thumbnail/width/360/height/360?cb=20190430112051");
-	});
+	server.on("/time", HTTP_GET, [](AsyncWebServerRequest *request)
+			  { request->send(200, "text/plain", getTimeF()); });
 
 	fastPrint(ALIGN_LEFT, 54, "Starting WebServer", u8g2_font_ncenB08_tr, false, true);
 
 	server.begin();
-	delay(100);
+	// delay(100);
 	loadAlarms(&Alarms);
 	nextAlarm = getNextAlarm(rtc.getTimeStruct(), Alarms);
-
-	Serial.println(SpiffsHelper::readFile("/Alarms.json"));
 }
 
 void loop()
@@ -384,14 +487,14 @@ void loop()
 	tm next = nextAlarm;
 
 	fastPrint(ALIGN_LEFT, 10, String("Time: ") + getTimeF(), u8g2_font_ncenB08_tr);
-	fastPrint(ALIGN_LEFT, 21, String("Alarm at: ") + String(nextAlarm.tm_hour) + ":" + String(nextAlarm.tm_min) + ":" + String(nextAlarm.tm_sec), u8g2_font_ncenB08_tr, false);
-	fastPrint(ALIGN_LEFT, 57, String("IP: ") + WiFi.localIP().toString(), u8g2_font_ncenB08_tr, false);
+	fastPrint(ALIGN_LEFT, 21, String("Irrigating at: ") + String(nextAlarm.tm_hour) + ":" + String(nextAlarm.tm_min) + ":" + String(nextAlarm.tm_sec), u8g2_font_ncenB08_tr, false);
+	fastPrint(ALIGN_LEFT, 57, String("IP: ") + WiFi.softAPIP().toString(), u8g2_font_ncenB08_tr, false);
 
 	if (compareAlarms(now, next))
 	{
-		fastPrint(0, 10, "ALARM", u8g2_font_ncenB08_tr, true, false);
+		fastPrint(0, 10, "Starting Water Pump", u8g2_font_ncenB08_tr, true, false);
 		fastPrint(0, 57, String("IP: ") + WiFi.localIP().toString(), u8g2_font_ncenB08_tr, false, true);
-		
+
 		digitalWrite(BUZZER, HIGH);
 		delay(200);
 		digitalWrite(BUZZER, LOW);
@@ -400,12 +503,16 @@ void loop()
 		delay(200);
 		digitalWrite(BUZZER, LOW);
 
+		digitalWrite(RELAY, LOW);
 		delay(DEFAULT_DURATION);
+		digitalWrite(RELAY, HIGH);
 
 		nextAlarm = getNextAlarm(rtc.getTimeStruct(), Alarms);
 	}
 
-	// Can do other task
+	// Can do other tasks
 
+#ifdef USE_OLED
 	u8g2.sendBuffer(); // prevent flickering
+#endif
 }
